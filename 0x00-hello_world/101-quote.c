ef2586a06af9bc9c4061d5c8befe18595fcb2fc9
Your Name <you@example.com>
2022-12-09 06:29:06 -0800

    Simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - prints a text
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+ {
+	 write(2, "and that piece of art is useful\ ' - samule jjc ,1997 to 2000\n",80);
+	 return (1);
+ 
+ }
+
