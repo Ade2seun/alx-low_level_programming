eb16d413978f1c86d1159bcf3f2c7d78fcf52721
Your Name <you@example.com>
2022-12-12 09:42:57 -0800

    The size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long long int c;
 char d;
 float f;
 
-Printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-Printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-Printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-Printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-Printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
