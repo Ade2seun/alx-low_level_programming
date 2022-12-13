51080a75fa0026144c4be4c6effd7ad9d1a1a9e6
Your Name <you@example.com>
2022-12-12 20:22:02 -0800

    Unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,10 @@ int main(void)
 	        putchar(c);
 		if (c != 57)
 		{
-			putchar(',');
-			putchar(' ');
+		       putchar(',');
+		       putchar(' ');
 		}
 	}
 	putchar('\n');
-	return (0);
+	return  (0);
 }
-
