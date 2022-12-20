065eafb9d4a6d867555a8e37f69e56deaf1aa008
Your Name <you@example.com>
2022-12-19 17:17:17 -0800

    The array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,8 +7,9 @@
 * @n: number of elements
 * Return: nothing
 */
- void print_array(int *a, int n)
-{ 
+
+void print_array(int *a, int n)
+{
 	int inc;
 
 	for (inc = 0; inc < n ; inc++)
@@ -17,6 +18,7 @@
 			printf("%d, ", a[inc]);
 		else
 			printf("%d", a[inc]);
+
 	}
 	putchar(10);
 }
