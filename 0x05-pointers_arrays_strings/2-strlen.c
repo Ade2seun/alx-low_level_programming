a1ec4041dd6578ce9628f59dc8ff6af60cde4e8f
Your Name <you@example.com>
2022-12-19 18:35:18 -0800

    The risk of beging read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "main.h"
 
 /**
  *_strlen - returns the length of a string
@@ -11,7 +10,7 @@ int _strlen(char *s)
 {
 	int len = 0;
 
-	while(*(s + len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (lens);
