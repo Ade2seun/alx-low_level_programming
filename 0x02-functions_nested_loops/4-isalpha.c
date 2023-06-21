e6cb52fe2e412a3b6b43de107f027f8672d22005
Ade2seun <adeseunjolade@gmail.com>
2023-06-20 22:07:31 -0700

    Is aphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - function to check if c is a letter , lowercase or uppercase
- * @c: is the int that will used for the argument of the function
+ * @c: Is the int that will used for the argument of the function
  * Return: 0
  */
 int _isalpha(int c)
