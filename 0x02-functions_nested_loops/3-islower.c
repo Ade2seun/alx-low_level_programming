3ad8a8f731ade9911086a666a744e84f3f8f71d7
Ade2seun <adeseunjolade@gmail.com>
2023-06-21 18:12:23 -0700

    World is lower and up

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - function to check for lowercase character
- * @C: is the int the will use the argument of the function
+ * @c: is the int the will use the argument of the function
  * Return: 0
  */
 int _islower(int c)
